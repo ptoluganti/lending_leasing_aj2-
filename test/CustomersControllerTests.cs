33f85562fa8c91c0cf74943cc8e3afa2b411fd54
Pradeep Toluganti <pradeep.toluganti@hotmail.com>
2016-08-12 14:19:43 +0100

    added test project

---
+++ test/CustomersControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using LendLease.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace LendLease.Tests
+{
+    public class CustomersControllerTests
+    {
+        public CustomersControllerTests()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        [Fact]
+        public async Task ReturnHelloWorld()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/Customers");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+
+            // Assert
+            var expectedString = JsonConvert.SerializeObject(new[] {"value1", "value2"});
+            Assert.Equal(expectedString, responseString);
+        }
+    }
+}
