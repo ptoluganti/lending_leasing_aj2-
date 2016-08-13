589beabc151c73fe17fc520d70fb7ac70e3d75d8
Pradeep Toluganti <pradeep.toluganti@hotmail.com>
2016-08-13 21:44:31 +0100

    added alert component

---
+++ src/LendLease.Web/ViewComponents/AlertViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LendLease.Web.ViewComponents
+{
+    public class AlertViewComponent: ViewComponent
+    {
+       
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = new List<string>();
+
+           
+            return View(viewModel);
+        }
+    }
+}
